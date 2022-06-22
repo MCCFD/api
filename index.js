dd8675c61ef9ebdebb9ea801254fc53dd9669bb8
qiaoshouzi <61568944+qiaoshouzi@users.noreply.github.com>
2022-06-23 00:47:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// 维护
+// app.use((req, res) => {
+//     res.setHeader('Content-Type', 'application/json;charset=utf-8');
+//     res.end(JSON.stringify({
+//         'code': 500,
+//         'msg': '维护中 预计 2022.06.23 08:00:00 前结束维护',
+//         'data': {}
+//     }));
+// });
+
 app.use('/api/login', login);
 app.use('/api/registered', registered);
 app.use('/api/find', find);
